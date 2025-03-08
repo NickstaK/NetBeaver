156a2562df6b83f6663c89b60d9a7f1278d21eb1
NickstaK <krier.nicholas@gmail.com>
2025-03-08 16:44:45 -0500

    Updated project files

---
+++ src/JAVA/NetBeaveR/src/main/java/com/mycompany/netbeaver/NetBeaveR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.netbeaver;
+
+/**
+ *
+ * @author NickstaK
+ */
+public class NetBeaveR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
